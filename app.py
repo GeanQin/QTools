c81366efa560539bcb3667f7c74921c4b7d9937b
geanqin <geanqin.chn@gmail.com>
2022-06-12 04:12:25 +0800

    add get_weather_icon

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 import time
+import requests
 
 app = Flask(__name__)
 
@@ -8,3 +9,24 @@ app = Flask(__name__)
 def get_time_stamp():
     t = time.time()
     return str(t)
+
+@app.route("/get_weather_icon")
+def get_weather_icon():
+    ip = request.remote_addr
+    ad_url = 'https://restapi.amap.com/v3/ip?key=e23f931ef9beee317fa5d06cbaaee882&ip=' + ip
+    ad_req = requests.get(ad_url)
+    ad_resp = ad_req.json()
+    weather_url = 'https://restapi.amap.com/v3/weather/weatherInfo?key=e23f931ef9beee317fa5d06cbaaee882&city=' + ad_resp["adcode"]
+    weather_req = requests.get(weather_url)
+    weather_resp = weather_req.json()
+    lives = weather_resp["lives"]
+    if "雨" in lives[0]["weather"]:
+        return str("rain")
+    elif "雪" in lives[0]["weather"]:
+        return str("rain")
+    elif "晴" in lives[0]["weather"]:
+        return str("sun")
+    elif "少云" in lives[0]["weather"]:
+        return str("sun")
+    else:
+        return str("cloud")
